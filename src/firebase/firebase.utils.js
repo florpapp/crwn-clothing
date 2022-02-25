2f33b57e62ce894357d159105b4d344ddc944748
flor <florpapp@gmail.com>
2022-02-25 15:13:12 -0300

    agrego sign up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,29 @@ const config = {
     measurementId: "G-ZYBWGKYEX6"
   
   };
+
+  export const createUserProfileDocument = async(userAuth, additionaData)=>{
+    if (!userAuth)return;
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists){
+      const { displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await userRef.set({
+          displayName, email, createdAt, ...additionaData
+        })
+      }
+      catch(error){
+        console.log('error when creating user', error.message);
+      }
+    }
+    return userRef;
+
+  }
   
   firebase.initializeApp(config);
 
