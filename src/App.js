bce8db49b9e065ec11b421777b5cf705dd191c98
flor <florpapp@gmail.com>
2022-03-10 12:22:27 -0300

    agrego checkout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser }  from './redux/user/user.selector';
 
 class App extends React.Component{
 
@@ -48,6 +52,8 @@ class App extends React.Component{
           <Switch>
             <Route exact path ='/' component={HomePage} />
             <Route path ='/shop' component={ShopPage} />
+            <Route exact path ='/checkout' component={CheckoutPage} />
+
             <Route exact path ='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInSignUpPage />)} />
 
           </Switch>    
@@ -57,8 +63,8 @@ class App extends React.Component{
 
 }
 
-const mapStateToProps = ({ user })=>({
-  currentUser:user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
